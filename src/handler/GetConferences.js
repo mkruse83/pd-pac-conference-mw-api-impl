3c4e48aed7375aa4728bd5b232e30f1131257bf1
Martin Kruse <mkruse@prodyna.com>
2019-05-14 23:44:29 +0200

    PAC-27 some adjustments to changed api

--- src/handler/GetConferences.js
+++ src/handler/GetConferences.js
@@ -3,11 +3,11 @@ const lambda = require("../helper/callLambda");
 
 class GetConferencesHandler {
   canHandle(event) {
-    return event.resource === "/conferences" && event.httpMethod === "GET";
+    return event.resource === "/conferences/{year}" && event.httpMethod === "GET";
   }
 
   handle(event) {
-    return lambda("getConferencesByYear", {year: Number.parseInt(event.queryStringParameters.year)})
+    return lambda("getConferencesByYear", {year: Number.parseInt(event.pathParameters.year)})
       .then(result => {
         return {
           statusCode: 200,
